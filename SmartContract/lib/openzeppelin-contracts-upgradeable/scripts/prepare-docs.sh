ce832e841c7297c5fbb4ed2c1d6bc4c72126175d
Zeegaths <zarahgathoni76@gmail.com>
2024-12-22 23:27:37 +0300

    readme

--- SmartContract/lib/openzeppelin-contracts-upgradeable/scripts/prepare-docs.sh
+++ SmartContract/lib/openzeppelin-contracts-upgradeable/scripts/prepare-docs.sh
@@ -1,26 +1,26 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-set -euo pipefail
-shopt -s globstar
+# set -euo pipefail
+# shopt -s globstar
 
-OUTDIR="$(node -p 'require("./docs/config.js").outputDir')"
+# OUTDIR="$(node -p 'require("./docs/config.js").outputDir')"
 
-if [ ! -d node_modules ]; then
-  npm ci
-fi
+# if [ ! -d node_modules ]; then
+#   npm ci
+# fi
 
-rm -rf "$OUTDIR"
+# rm -rf "$OUTDIR"
 
-hardhat docgen
+# hardhat docgen
 
-# copy examples and adjust imports
-examples_source_dir="contracts/mocks/docs"
-examples_target_dir="docs/modules/api/examples"
+# # copy examples and adjust imports
+# examples_source_dir="contracts/mocks/docs"
+# examples_target_dir="docs/modules/api/examples"
 
-for f in "$examples_source_dir"/**/*.sol; do
-  name="${f/#"$examples_source_dir/"/}"
-  mkdir -p "$examples_target_dir/$(dirname "$name")"
-  sed -Ee '/^import/s|"(\.\./)+|"@openzeppelin/contracts/|' "$f" > "$examples_target_dir/$name"
-done
+# for f in "$examples_source_dir"/**/*.sol; do
+#   name="${f/#"$examples_source_dir/"/}"
+#   mkdir -p "$examples_target_dir/$(dirname "$name")"
+#   sed -Ee '/^import/s|"(\.\./)+|"@openzeppelin/contracts/|' "$f" > "$examples_target_dir/$name"
+# done
 
-node scripts/gen-nav.js "$OUTDIR" > "$OUTDIR/../nav.adoc"
+# node scripts/gen-nav.js "$OUTDIR" > "$OUTDIR/../nav.adoc"
